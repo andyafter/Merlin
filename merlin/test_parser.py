03d416019b2a1ff7383190e39e8cf04eebce51ac
anpan <anpan@airasia.com>
2020-05-27 23:29:27 +0700

    Added test

--- merlin/test_parser.py
+++ merlin/test_parser.py
@@ -5,6 +5,13 @@ from importlib import import_module
 from merlin.parser import MetricParser
 
 
+# TODO: make sure that this path aligns with the
+EXTRA_LIB_PATH = "/modules.zip"
+if os.path.exists(EXTRA_LIB_PATH):
+    sys.path.append(EXTRA_LIB_PATH)
+
+import DefaultStage
+
 class TestParser(unittest.TestCase):
     def test_something(self):
         MetricParser().load_metrics(metric_db="yaml/metric_definition.yml")
